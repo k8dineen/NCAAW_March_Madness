9911c3d3e5e9d0055e36afe60edb3af52f7df6c6
k8dineen <kathryn.dineen@columbia.edu>
2025-03-20 14:43:27 -0400

    Removing old backend to start fresh with tutorial. Added proper Vue frontend.

---
+++ front-end/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/ncaaw_logo.webp">
+  <HelloWorld msg="Welcome to the 2025 Women's College Basketball March Madness"/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
